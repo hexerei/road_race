87a9067d12521ab5ca6e061692f8748b92d01cd7
Daniel Vorhauer <daniel.vorhauer@dxc.com>
2023-01-31 16:33:15 +0100

    keyboard handling and player movement

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use rusty_engine::prelude::*;
 
+const PLAYER_SPEED: f32 = 250.0; // pixel per seconds
+const WINDOW_WIDTH: f32 = 800.0;
+const WINDOW_HEIGHT: f32 = 300.0;
+
 struct GameState {
     health_amount: u8,
     lost: bool,
@@ -11,14 +15,14 @@ fn main() {
     // game setup goes here
     game.window_settings(WindowDescriptor {
         title: "Road Racer".into(),
-        width: 800.0,
-        height: 300.0,
+        width: WINDOW_WIDTH,
+        height: WINDOW_HEIGHT,
         ..Default::default()
     });
 
     // create player sprite
     let player1 = game.add_sprite("player1", SpritePreset::RacingCarBlue);
-    player1.translation.x = -300.0;
+    player1.translation.x = -(WINDOW_WIDTH / 2.0 - 100.0);
     player1.layer = 10.0;
     player1.collision = true;
 
@@ -34,5 +38,20 @@ fn main() {
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
-    // game logic goes here
+    // Collect keyboard input
+    let mut direction = 0.0;
+    if engine.keyboard_state.pressed(KeyCode::Left) {
+        direction += 1.0;
+    }
+    if engine.keyboard_state.pressed(KeyCode::Right) {
+        direction -= 1.0;
+    }
+
+    // Move the player sprite
+    let player1 = engine.sprites.get_mut("player1").unwrap();
+    player1.translation.y += direction * PLAYER_SPEED * engine.delta_f32;
+    player1.rotation = direction * 0.15;
+    if player1.translation.y < -(WINDOW_HEIGHT / 2.0) || player1.translation.y > WINDOW_HEIGHT / 2.0 {
+        game_state.health_amount = 0;
+    }
 }
